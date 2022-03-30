dbfa7fd7187fe61e74eec57756b37d22376b46bc
cperryoh <46071730+cperryoh@users.noreply.github.com>
2022-03-30 00:06:14 -0400

    Comments

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -31,9 +31,16 @@ public class Scraper {
             float currentPrice;
             float percentMovement;
             String companyName;
+
+            //For most of the numerical values, I have to remove the commas, I don't know if float.parse is ok with them, but i assumed it was not and removed them
+
+
+            //pull html from web
             TagNode tagNode = new HtmlCleaner(props).clean(
                     new URL("https://finance.yahoo.com/quote/"+ticker.toUpperCase()+"?p="+ticker.toUpperCase()+"&.tsrc=fin-srch")
             );
+
+            //parse prev close and convert to float
             TagNode prevCloseNode = evalPath(tagNode,"//*[@id=\"quote-summary\"]/div[1]/table/tbody/tr[1]/td[2]");
             try {
                 prevClose = Float.parseFloat(prevCloseNode.getText().toString().replaceAll(",", ""));
@@ -41,6 +48,7 @@ public class Scraper {
                 prevClose= 0;
             }
 
+            //parse open price and convert to float
             TagNode openNode = evalPath(tagNode,"//*[@id=\"quote-summary\"]/div[1]/table/tbody/tr[2]/td[2]");
             try {
                 open = Float.parseFloat(openNode.getText().toString().replaceAll(",", ""));
@@ -48,8 +56,8 @@ public class Scraper {
                 open= 0;
             }
 
-            // //*[@id="quote-summary"]/div[2]/table/tbody/tr[3]/td[2]
 
+            //parse pe ratio and convert to float
             TagNode peRatioNode = evalPath(tagNode,"//*[@id=\"quote-summary\"]/div[2]/table/tbody/tr[3]/td[2]");
             try {
                 peRatio = Float.parseFloat(peRatioNode.getText().toString().replaceAll(",", ""));
@@ -57,15 +65,24 @@ public class Scraper {
                 peRatio= 0;
             }
 
+            //parse current price
             TagNode currentPriceNode = evalPath(tagNode,"//*[@id=\"quote-header-info\"]/div[3]/div[1]/div[1]/fin-streamer[1]");
             currentPrice=Float.parseFloat(currentPriceNode.getText().toString().replaceAll(",",""));
 
-
+            //parse current percentage movement
+            // for this one the xpath was pissy for whatever reason
+            // I used a different feature called find element by attribute
+            //I chose a unique attribute on the html node i was trying to reach and pulled it via that instead of xpath
+            //I also had to remove a lot of characters from the string it comes in as (+1.00%), so i removed
+            //the +,(,), and % characters so Float.parseFloat could make sense of it
             TagNode percentMovementNode = (TagNode) tagNode.findElementByAttValue("data-field", "regularMarketChangePercent", true, true).getAllChildren().get(0);
             percentMovement=Float.parseFloat(percentMovementNode.getText().toString().replaceAll(",","").replace("(","").replace(")","").replace("+","").replace("%",""));
 
+            //parse full company name
             TagNode companyNameNode = evalPath(tagNode,"//*[@id=\"quote-header-info\"]/div[2]/div[1]/div[1]/h1");
             companyName=companyNameNode.getText().toString();
+
+            //pass all that data into a class and return
             return new StockData(prevClose,open,peRatio,currentPrice,percentMovement,ticker,companyName);
         } catch (Exception e){
             e.printStackTrace();
